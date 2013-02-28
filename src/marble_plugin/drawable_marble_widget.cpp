4a0e7ae96f43bc3ae00b86b1a56b8121b27f8706
Jan Aidel <aiden@fzi.de>
2013-02-28 12:03:23 +0100

    show car direction

--- src/marble_plugin/drawable_marble_widget.cpp
+++ src/marble_plugin/drawable_marble_widget.cpp
@@ -13,7 +13,6 @@ DrawableMarbleWidget::DrawableMarbleWidget(QWidget *parent)
     QString qpath(path.c_str());
     QImage image(qpath);
     m_car = image;
-
 }
 
 
@@ -33,25 +32,16 @@ void DrawableMarbleWidget::receiveLastPosition(GeoDataCoordinates &postion)
 QImage DrawableMarbleWidget::roateCar(QImage *image)
 {
     //we can do this because of really smal changes between last and actual position
-    double x1 = m_last_position.latitude();
-    double x2 = m_actual_position.latitude();
-    double y1 = m_last_position.longitude();
-    double y2 = m_last_position.longitude();
+    double x1 = m_last_position.latitude(GeoDataCoordinates::Radian);
+    double x2 = m_actual_position.latitude(GeoDataCoordinates::Radian);
 
-    std::cout << "x1 " << x1 << " y1 " << y1 << " x2 " << x2 << " y2 " << y2 << std::endl;
+    double y1 = m_last_position.longitude(GeoDataCoordinates::Radian);
+    double y2 = m_actual_position.longitude(GeoDataCoordinates::Radian);
 
-    //since for smal angle: sin(angle) = angle => compute sin and assume as angle
-    double hyp = dist(x1, y1, x2, y2);
-    double cat = dist(x2, y1, x2, y2);
-
-    std::cout << " hyp " << hyp << " cat " << cat << std::endl;
-
-    double sin = cat/hyp;
-
-    std::cout << "sin " << sin << std::endl;
+    double alpha = atan2(y2-y1,x2-x1)*180*M_1_PI;
 
     QTransform rotaion;
-    rotaion.rotate(sin);
+    rotaion.rotate(alpha);
     return image->transformed(rotaion);
 }
 
